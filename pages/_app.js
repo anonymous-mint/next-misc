d18547746545c7137ebc1c98e075cb0bedd47166
anonymous-mint <annonymuskmint@gmail.com>
2022-10-28 02:22:11 +0530

    Head component

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,23 @@ import Header from "../components/header";
 import "../styles/globals.css";
 import "../styles/layout.css";
 import Footer from "../components/footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   console.log(Component);
+
+  //function defined in the about component is checked here and layout is applied
   if (Component.getLayout)
-    return Component.getLayout(<Component {...pageProps} />);
+    return Component.getLayout(
+      <>
+      {/* Head is updated here */}
+        <Head>
+          <title>Next Mics</title>
+          <meta name="Home" content="nice head" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
   // global layout for all the pages under _app
   return (
     <>
